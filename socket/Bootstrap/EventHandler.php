096d644eea06349396fc418cc215c7329158e01d
uzdevid <uzdevid@gmail.com>
2024-05-18 01:51:41 +0500

    Доработки

--- socket/Bootstrap/EventHandler.php
+++ socket/Bootstrap/EventHandler.php
@@ -5,8 +5,10 @@ namespace socket\Bootstrap;
 use common\Model\Sharing;
 use UzDevid\WebSocket\Server\Event\CloseConnection;
 use UzDevid\WebSocket\Server\Event\NewConnection;
+use UzDevid\WebSocket\Server\Event\NewRawMessage;
 use yii\base\BootstrapInterface;
 use yii\helpers\Console;
+use yii\helpers\Json;
 
 class EventHandler implements BootstrapInterface {
     /**
@@ -19,9 +21,12 @@ class EventHandler implements BootstrapInterface {
             Console::stdout("New Connection {$event->client->id}\r\n");
         });
 
-        $app->on(CloseConnection::class, function (CloseConnection $event) {
-            Console::stdout("Close Connection {$event->client->id}\r\n");
+        $app->on(NewRawMessage::class, function (NewRawMessage $message) {
+            Console::stdout(Json::encode($message->message));
+            Console::stdout("\r\n------------------------\r\n");
+        });
 
+        $app->on(CloseConnection::class, function (CloseConnection $event) {
             $sharing = Sharing::find()->where(['connection_id' => $event->client->id])->one();
 
             if (is_null($sharing)) {
