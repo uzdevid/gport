8bb3152b9c40e732a43284459aee96819f030e52
uzdevid <uzdevid@gmail.com>
2024-05-19 01:21:12 +0500

    Доработки

--- proxy/Controller/AppController.php
+++ proxy/Controller/AppController.php
@@ -35,7 +35,7 @@ class AppController extends Controller {
             'request' => [
                 'method' => Yii::$app->request->method,
                 'headers' => Yii::$app->request->headers->toArray(),
-                'body' => Yii::$app->request->post()
+                'body' => Yii::$app->request->post() ?? null
             ]
         ]);
 
