876613eef0eac946ca0cc898c1931d9e0aebe87a
uzdevid <uzdevid@gmail.com>
2024-05-18 23:00:47 +0500

    Доработки

--- proxy/Controller/AppController.php
+++ proxy/Controller/AppController.php
@@ -27,12 +27,6 @@ class AppController extends Controller {
 
         $client = Yii::$app->webSocketClient;
 
-//        Yii::$app->queue->push(new CallLocalAddress($client, [
-//            'connectionId' => $sharing->connection_id,
-//            'requestId' => $requestId,
-//            'address' => $sharing->local_address . $_SERVER['REQUEST_URI'],
-//        ]));
-
         $client->send('local-client:call-address', [
             'connectionId' => $sharing->connection_id,
             'requestId' => $requestId,
