7ecfd25ed89ebacc517caa360cd129110f3c27c2
uzdevid <uzdevid@gmail.com>
2024-05-18 22:35:04 +0500

    Доработки

--- socket/Controller/SharingController.php
+++ socket/Controller/SharingController.php
@@ -20,7 +20,7 @@ class SharingController extends Controller {
         $sharing->key = strtolower(Yii::$app->security->generateRandomString(4));
         $sharing->user_id = Uuid::uuid4()->toString();
         $sharing->connection_id = $client->id;
-        $sharing->remote_address = "gport.web";
+        $sharing->remote_address = sprintf('%s.gport.uzdevid.com', $sharing->key);
         $sharing->local_address = $payload['localAddress'];
         $sharing->active = 0;
         $sharing->is_active = true;
